33e8d8629b3d32973bcfe4a32bb7de5d78c65e14
Erin Knowles <erinknowles@protonmail.com>
2023-01-20 15:53:51 -0500

    Revert "Test"
    This reverts commit f82e9312e223dea30d1b9c5a2e4ef37cfba9f1ac.

---
+++ src/ui/consts.js
@@ -0,0 +1,14 @@
+export const SELECTOR_CHARS = { class: ".", id: "#" };
+
+export const DISPLAY_COLORS = [
+    "red",
+    "yellow",
+    "blue",
+    "green",
+    "purple",
+    "orange",
+    "cyan",
+    "hotpink",
+    "indigo",
+    "limegreen",
+];
