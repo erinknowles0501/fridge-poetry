9ca85316bd1ac99462378f3009e9185941b37e6b
Erin Knowles <erinknowles@protonmail.com>
2023-01-24 12:41:04 -0500

    Pass port to server from build script.

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import polka from "polka";
 import sirv from "sirv";
 
+// TODO Build obj with keys/values and get port from there
+const port = process.argv[2].split("=")[1];
+
 polka()
     .use(sirv(".", { dev: true, single: true }))
-    .get("/", (req, res) => {
-        res.end();
-    })
-    .listen(7020, () => {});
+    // .get("/", (req, res) => {
+    //     res.end();
+    // })
+    // .get("/", (req, res) => {
+    //     res.end();
+    // })
+    .listen(port ?? 7020, () => {});
--- src/store.js
+++ src/store.js
@@ -11,24 +11,28 @@ class Store {
     router = null;
 
     async initialize(services) {
-        this.clear();
+        return new Promise(async (resolve) => {
+            this.clear();
 
-        this.services = services;
-        this.appEl = document.querySelector("#app");
+            this.services = services;
+            this.appEl = document.querySelector("#app");
 
-        this.fridge.fridgeID = window.location.pathname.slice(1);
+            this.fridge.fridgeID = window.location.pathname.slice(1);
 
-        this.fridge.info = await this.services.fridgeService.getFridgeByID(
-            this.fridge.fridgeID
-        );
-        this.fridge.words = await this.services.wordService.getWordsByFridge(
-            this.fridge.fridgeID
-        );
+            this.fridge.info = await this.services.fridgeService.getFridgeByID(
+                this.fridge.fridgeID
+            );
+            this.fridge.words =
+                await this.services.wordService.getWordsByFridge(
+                    this.fridge.fridgeID
+                );
 
-        await this.services.authService.signIn();
-        this.user = await this.services.userService.getUserByID(
-            this.services.authService.auth.currentUser.uid
-        );
+            await this.services.authService.signIn();
+            this.user = await this.services.userService.getUserByID(
+                this.services.authService.auth.currentUser.uid
+            );
+            resolve();
+        });
     }
 
     clear() {
