4d8b07a755818545d290d01e21814dcdda128062
Erin Knowles <erinknowles@protonmail.com>
2023-01-26 17:51:06 -0500

    Small rework of firebase rewrites rules to better allow js+css through. Modify rollup config to resolve 'vue' to the cdn.

--- rollup.config.js
+++ rollup.config.js
@@ -7,7 +7,14 @@ export default {
         fridge: "src/fridge/index.js",
         front: "src/front/index.js",
     },
-    output: { dir: "public", entryFileNames: "[name]-bundle.js" },
+    output: {
+        dir: "public",
+        entryFileNames: "[name]-bundle.js",
+        paths: {
+            vue: "https://unpkg.com/vue@3/dist/vue.esm-browser.js",
+        },
+    },
     plugins: [nodeResolve(), commonjs(), json()],
+    external: ["vue"],
     watch: true,
 };
