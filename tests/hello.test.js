2532cda4c3a5fb53ca88b48d1fc989379033a570
Erin Knowles <erinknowles@protonmail.com>
2023-02-11 00:20:57 -0500

    Destructure default import.

--- tests/hello.test.js
+++ tests/hello.test.js
@@ -2,14 +2,14 @@ import { jest } from "@jest/globals";
 jest.unstable_mockModule("./hello", () => ({
     default: jest.fn((person) => "hello " + person + "!"),
 }));
-const hello = await import("./hello");
+const { default: hello } = await import("./hello");
 
-console.log("hello", hello.default);
+console.log("hello", hello);
 
 test("Hello is mocked", async () => {
-    expect(jest.isMockFunction(hello.default)).toEqual(true);
+    expect(jest.isMockFunction(hello)).toEqual(true);
 });
 
 test("Says hello to Erin", () => {
-    expect(hello.default("Erin")).toBe("hello Erin!");
+    expect(hello("Erin")).toBe("hello Erin!");
 });
