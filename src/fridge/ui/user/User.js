1e65742ee842a04dab9325d6b56ceb6444c645c3
Erin Knowles <erinknowles@protonmail.com>
2023-03-20 00:20:52 -0400

    Fix margin CSS bug on .menu / .user-menu

--- src/fridge/ui/user/User.js
+++ src/fridge/ui/user/User.js
@@ -6,7 +6,7 @@ export default {
     inject: ["navigate", "store"],
     components: { UserColorDisplay },
     template: `
-    <h3 class="user-name"><UserColorDisplay/>{{store.scale.isPortrait ? store.user?.displayName : ''}}</h3>
+    <h3 class="user-name" :style="!isOpen && 'margin-top: 0rem'"><UserColorDisplay/>{{!store.scale.isPortrait && !isOpen ? '' : store.user?.displayName}}</h3>
     <div class="user">
         <div class="menu" v-if="isOpen">
             <a v-for="link in menuItems" @click.prevent="navigate(link)" href="#">{{link.title}}</a>
